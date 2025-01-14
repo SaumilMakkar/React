5ba00ccb0becfa344e3b87864b0bbda9241a77d4
Saumil Makkar <saumilmakkar@gmail.com>
2025-01-14 18:33:22 +0530

    "Committing rest of the code"

--- 01-basicvite/src/main.jsx
+++ 01-basicvite/src/main.jsx
@@ -1,10 +1,36 @@
 import { StrictMode } from 'react'
+import React from 'react'  
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+const reactElement={
+  type:'a',
+  props:{
+      href:"https://google.com",
+      target:'_blank'
+  },
+  children:'Click me to visit google'
+}
+function MyApp(){
+  return (
+    <>
+    <h1>Custom React App</h1>
+
+   
+    </>
+  )
+  
+}
+const AnotherElement=(
+  <a href="https:google.com" target='_blank'>Visit Google</a>
+)
+const areactElement=React.createElement(
+
+  'a',
+  {href:'https://google.com',target:"_blank"},
+  'click to visit google'
+)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ areactElement
 )
